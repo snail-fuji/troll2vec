d44dded307b60a4c4045fdfd8a59f7467c3c5ec2
Anatoli Belchikov <belchikov1996@gmail.com>
2019-02-25 00:29:52 +0300

    Added fast distance computations

--- tests/levenschtein_tests.py
+++ tests/levenschtein_tests.py
@@ -12,7 +12,7 @@ class LevenschteinTestCase(unittest.TestCase):
     all_tokens = words.words()
     partial_tokens = {random.choice(all_tokens): index for index in range(50000)}
     partial_tokens_inv = {i: v for v, i in partial_tokens.items()}
-    typos.init(partial_tokens)
+    typos.init(partial_tokens, test=True)
     word_pairs = []
 
     for index in tqdm(range(100)):
@@ -22,3 +22,25 @@ class LevenschteinTestCase(unittest.TestCase):
 
     print(word_pairs)
     print(np.mean([fuzz.ratio(word1, word2) for word1, word2 in word_pairs]))
+
+  def test_large_index_for_jaccard(self):
+    all_tokens = words.words()
+    partial_tokens = {random.choice(all_tokens): index for index in range(50000)}
+    typos.init(partial_tokens, test=True)
+
+    for index in tqdm(range(100)):
+        word = random.choice(all_tokens)
+        typos.find_closest_by_jaccard(word)
+
+  def test_create_jaccard_index(self):
+    words = ["ABC", "AB", "AC", "AAB"]
+    _, index_matrix = typos.create_jaccard_index(words)
+    index_matrix = index_matrix.todense()
+    print(index_matrix)
+    assert (index_matrix == np.matrix([[1, 1, 1], [1, 1, 0], [1, 0, 1], [1, 1, 0]])).all()
+  
+  def test_find_closest_by_jaccard(self):
+    words = {word: index for index, word in enumerate(["ABC", "AB", "AC", "AAB"])}
+    typos.init(words, test=True)
+    result = typos.find_closest_by_jaccard("ABD")
+    print(result)
--- typos/typos.py
+++ typos/typos.py
@@ -1,28 +1,69 @@
 import distance
+import numpy as np
+import scipy.sparse as sps
+from collections import Counter
 
 vocabulary = None
 all_words = None
 
-def jaccard(set1, set2):
-    return len(set1.intersection(set2)) / len(set1.union(set2))
+unique_characters = None
+jaccard_index = None
+jaccard_index_lengths = None
 
-def init(init_vocabulary):
-    global vocabulary, all_words, all_sets
-    if not vocabulary:
+def get_word_indices(word, unique_characters):
+  char_indices = []
+  for character in word:
+    if character in unique_characters:
+      char_indices.append(unique_characters[character])
+  return char_indices
+
+def create_jaccard_index(words):
+  all_characters = [character for word in words for character in word]
+  unique_characters = {
+    character[0]: index 
+    for index, character in enumerate(Counter(all_characters).most_common())
+  }
+
+  rows = []
+  columns = []
+
+  for index, word in enumerate(words):
+    extracted_columns = get_word_indices(word, unique_characters)
+    columns += extracted_columns
+    rows += [index] * len(extracted_columns)
+
+  data = np.ones(len(columns))
+
+  return unique_characters, sps.coo_matrix(
+    (data, (rows, columns)), 
+    shape=(len(words), len(unique_characters))
+  ).astype(bool).astype(int)
+
+def init(init_vocabulary, test=False):
+    global vocabulary, all_words, jaccard_index, soundex_words, unique_characters, jaccard_index_lengths
+    if (not vocabulary) or test:
       vocabulary = init_vocabulary
       all_words = list(vocabulary.keys())
-      all_sets = [set(word) for word in vocabulary]
+      unique_characters, jaccard_index = create_jaccard_index(all_words)
+      jaccard_index_lengths = sps.linalg.norm(jaccard_index, axis=1)
 
-def find_closest_by_jaccard(word, size=1000):
-    global all_sets, all_words
-    word_set = set(word)
-    return [
-      word 
-      for jaccard, word in sorted([
-        (-jaccard(word_set, vocabulary_word_set), vocabulary_word) 
-        for vocabulary_word_set, vocabulary_word in zip(all_sets, all_words)
-      ])
-    ][0:size]
+def find_closest_by_jaccard(word, size=100):
+    global jaccard_index, all_words, unique_characters, jaccard_index_lengths
+
+    chars_indices = get_word_indices(word, unique_characters)
+    word_indices = np.zeros(len(chars_indices))
+    data = np.ones(len(chars_indices))
+
+    word_vector = sps.coo_matrix(
+      (data, (word_indices, chars_indices)), 
+      shape=(1, len(unique_characters))
+    ).astype(bool).astype(int)
+
+    intersections = word_vector.dot(jaccard_index.T)
+    word_length = sps.linalg.norm(word_vector)
+    index_lengths = jaccard_index_lengths
+    cosine_distances = intersections / (word_length * index_lengths) 
+    return [all_words[index] for index in np.argsort(-cosine_distances).tolist()[0][0:size]]
 
 def find_closest_by_levenshtein(word, selected_words):
     distances = list(distance.ifast_comp(word, selected_words))
